1c36c56639f8916084ba682d948659de979f2bfb
David Aparicio <david.aparicio@ovhcloud.com>
2023-03-14 14:21:36 +0100

    👷 ci: GolangCI-Lint

--- examples/go_build.sh
+++ examples/go_build.sh
@@ -4,6 +4,8 @@ VERSION=$(git name-rev --tags --name-only "$(git rev-parse HEAD)")
 GIT_COMMIT=$(git rev-list -1 HEAD)
 BUILT_DATE=$(date)
 PACKAGE="github.com/davidaparicio/cuc/internal"
+#GO111MODULE="" "on"
+#TZ="Europe/Paris"
 
 LDFLAGS="-s -w -X '${PACKAGE}.Version=${VERSION}' -X '${PACKAGE}.GitCommit=${GIT_COMMIT}' -X '${PACKAGE}.BuiltDate=${BUILT_DATE}'"
 CGO_ENABLED=1
