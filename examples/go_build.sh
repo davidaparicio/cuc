4a8647d4105a32b28ebac6168bfd136eb9d3077a
David Aparicio <david.aparicio@free.fr>
2023-03-15 17:24:30 +0100

    👷 ci: makefile

--- examples/go_build.sh
+++ examples/go_build.sh
@@ -2,12 +2,12 @@
 
 VERSION=$(git name-rev --tags --name-only "$(git rev-parse HEAD)")
 GIT_COMMIT=$(git rev-list -1 HEAD)
-BUILT_DATE=$(date)
+BUILD_DATE=$(date)
 PACKAGE="github.com/davidaparicio/cuc/internal"
 #GO111MODULE="" "on"
 #TZ="Europe/Paris"
 
-LDFLAGS="-s -w -X '${PACKAGE}.Version=${VERSION}' -X '${PACKAGE}.GitCommit=${GIT_COMMIT}' -X '${PACKAGE}.BuiltDate=${BUILT_DATE}'"
+LDFLAGS="-s -w -X '${PACKAGE}.Version=${VERSION}' -X '${PACKAGE}.GitCommit=${GIT_COMMIT}' -X '${PACKAGE}.BuildDate=${BUILD_DATE}'"
 CGO_ENABLED=1
 TARGETOS="darwin" #GOOS=linux or GOOS=windows
 TARGETARCH="amd64" #go env 32bit: GOARCH=386
@@ -17,4 +17,4 @@ TARGETARCH="amd64" #go env 32bit: GOARCH=386
 CGO_ENABLED="${CGO_ENABLED}" GOOS="${TARGETOS}" GOARCH="${TARGETARCH}" \
 go build -ldflags \
 "${LDFLAGS}" \
--a -installsuffix cgo -o ./cmd/cuc .
+-a -installsuffix cgo -o ./bin/cuc .
